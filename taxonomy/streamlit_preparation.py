c739da01c176417c1a7158bf122e970dc0fc4563
frasalvi <salvi.fnc@gmail.com>
2022-05-16 23:05:43 +0200

    static app

---
+++ taxonomy/streamlit_preparation.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+from tqdm import tqdm
+from utilities import init_logger, printt
+from queryLabel import Taxonomy
+
+GRAPH_PATH = '/scratch/WikipediaImagesTaxonomy/20220220-category-graph.pkl.bz2'
+FILES_PATH = '/scratch/WikipediaImagesTaxonomy/commonswiki-20220220-files.parquet'
+SAMPLE_PATH = 'streamlit_data/'
+LOG_PATH = 'streamlit_preparation.log'
+
+
+logger = init_logger(LOG_PATH, logger_name='taxonomy')
+logfile = open(LOG_PATH, 'w+')
+
+
+def initialize():
+    printt('Reading files...')
+    files = pd.read_parquet(FILES_PATH)
+
+    taxonomy = Taxonomy()
+    printt('Loading graph...')
+    taxonomy.load_graph(GRAPH_PATH)
+    printt('Loading mapping...')
+    taxonomy.set_taxonomy(mapping='content_extended')
+    printt('Loading lexical parser...')
+    taxonomy.get_head('CommonsRoot')
+
+    return files, taxonomy
+
+
+def get_sample(n, seed):
+    return files.sample(n, random_state=seed)
+
+
+def queryFile(file, how='heuristics'):
+    '''
+    Given one file, a row of the files DataFrame, queries recursively all
+    the categories and returns the final labels.
+    '''
+
+    labels = set()
+    for category in file.categories:
+        logger.debug(f'Starting search for category {category}')
+        cat_labels = taxonomy.get_label(category, how=how)
+        logger.debug(f'Ending search for category {category} with resulting labels {cat_labels}')
+        logger.debug(f'---------------------------------------------------')
+        labels |= cat_labels
+    logger.debug(f'Final labels: {labels}')
+    log = logfile.read()
+    return labels, log
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--n', help='size of the sample')
+    parser.add_argument('-s', '--seed', help='random seed')
+    args = parser.parse_args()
+
+    files, taxonomy = initialize()
+
+    n = int(args.n) if args.n else 1000
+    seed = int(args.seed) if args.seed else 0
+    files_sample = get_sample(n, seed)
+    tqdm.pandas()
+    files_sample[['labels', 'log']] = files_sample.progress_apply(lambda x: queryFile(x), 
+                                                                  axis=1, result_type="expand")
+    files_sample.to_parquet(SAMPLE_PATH + f'files_{seed}.{n}.parquet')
---
+++ taxonomy/streamlit_static.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import os
+
+SAMPLE_PATH = 'streamlit_data/'
+COMMONS_URL = 'https://commons.wikimedia.org/wiki/File:'
+UPLOAD_URL = 'https://upload.wikimedia.org/wikipedia/commons/'
+
+
+def showFile():
+    '''
+    Show the current file, printing the labels and the log.
+    '''
+
+    file = st.session_state.files.iloc[st.session_state.fileList[st.session_state.counter]]
+
+    commons_link = COMMONS_URL + file.url.split('/')[-1]
+    image_link = UPLOAD_URL + file.url
+
+    # Display the image with a link to commons
+    st.subheader(file.title)
+    st.markdown(f'<a href="{commons_link}"><img src="{image_link}"' +
+                 ' alt="drawing" width="600"/>', unsafe_allow_html=True)
+
+    st.subheader('Labels: [' + ', '.join(file.labels) + ']')
+    
+    # Extract the current log
+    log_dump = file.log.replace('\n', '  \n ')
+    with st.expander("Show log"):
+        st.write(log_dump)
+
+
+def nextFile():
+    if(st.session_state.counter == len(st.session_state.fileList) - 1):
+        st.session_state.fileList.append(np.random.randint(0, len(st.session_state.files)))
+
+    st.session_state.counter += 1
+    st.session_state.previous_disabled = not bool(st.session_state.counter)
+    showFile()
+
+
+def previousFile():
+    st.session_state.counter -= 1
+    if(not st.session_state.counter):
+        st.session_state.previous_disabled = True
+    showFile()
+
+
+def resetSeedNumber():
+    np.random.seed(st.session_state.seedNumber)
+    st.session_state.fileList = []
+    st.session_state.previous_disabled = True
+    st.session_state.counter = -1
+    nextFile()
+
+
+def load_dataset():
+    with st.spinner('Loading files...'):
+        st.session_state.files = pd.read_parquet(SAMPLE_PATH + st.session_state.dataset)
+        st.session_state.filesize = len(st.session_state.files)
+    resetSeedNumber()
+
+
+# Initialize session
+if 'seedNumber' not in st.session_state:       
+    # Other initializations
+    st.session_state.seedNumber = 0
+    st.session_state.dataset = 'files_0.10.parquet'
+    load_dataset()
+
+
+col1, col2, col3, col4 = st.columns([1, 1, 1.8, 1.8])
+col1.text(""); col1.text("");
+col1.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
+col2.text(""); col2.text("");
+col2.button('Next file', on_click=nextFile)
+col3.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
+col4.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
+col4.button('Reset', on_click=resetSeedNumber)
