d5997246cfedd177cffee8c32d21e37f1f8ed060
frasalvi <salvi.fnc@gmail.com>
2022-05-22 20:59:37 +0200

    evaluation suite, sidebar

--- taxonomy/streamlit_static.py
+++ taxonomy/streamlit_static.py
@@ -22,9 +22,26 @@ def showFile():
     st.subheader(file.title)
     st.markdown(f'<a href="{commons_link}"><img src="{image_link}"' +
                  ' alt="drawing" width="600"/>', unsafe_allow_html=True)
-
-    st.subheader('Labels: [' + ', '.join(file.labels) + ']')
     
+    
+    st.subheader('Labels: [' + ', '.join(file.labels) + ']')
+
+    # Hide first botton of radio widget, so that by default no option is selected
+    st.markdown(
+    """ <style>
+            div[role="radiogroup"] >  :first-child{
+                display: none !important;
+            }
+        </style>
+        """,
+    unsafe_allow_html=True
+    )
+    index_map = {-1: 0, 0: 2, 1: 1}
+    for label, value in file.labels.items():
+        st.radio(label, ('-', 'Correct', 'Wrong'), key=file.title + label, index=index_map[value], 
+                 on_change=evaluate_label, args=(file, label))
+
+
     # Extract the current log
     log_dump = file.log.replace('\n', '  \n ')
     with st.expander("Show log"):
@@ -57,24 +74,39 @@ def resetSeedNumber():
 
 def load_dataset():
     with st.spinner('Loading files...'):
-        st.session_state.files = pd.read_parquet(SAMPLE_PATH + st.session_state.dataset)
+        st.session_state.files = pd.read_json(SAMPLE_PATH + st.session_state.dataset)
         st.session_state.filesize = len(st.session_state.files)
     resetSeedNumber()
 
 
+def evaluate_label(file, label):
+    index_map = {'Correct': 1, 'Wrong': 0}
+    file.labels[label] = index_map[st.session_state[file.title + label]]
+    showFile()
+
+
 # Initialize session
 if 'seedNumber' not in st.session_state:       
-    # Other initializations
+    # st.set_page_config(layout="wide")
     st.session_state.seedNumber = 0
-    st.session_state.dataset = 'files_0.10.parquet'
+    st.session_state.dataset = 'files_0.10.json.bz2'
     load_dataset()
 
 
-col1, col2, col3, col4 = st.columns([1, 1, 1.8, 1.8])
-col1.text(""); col1.text("");
-col1.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
-col2.text(""); col2.text("");
-col2.button('Next file', on_click=nextFile)
-col3.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
-col4.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
-col4.button('Reset', on_click=resetSeedNumber)
+
+# col1, col2, col3, col4 = st.columns([1, 1, 1.8, 1.8])
+# col1.text(""); col1.text("");
+# col1.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
+# col2.text(""); col2.text("");
+# col2.button('Next file', on_click=nextFile)
+# col3.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
+# col4.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
+# col4.button('Reset', on_click=resetSeedNumber)
+
+with st.sidebar:
+    st.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
+    st.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
+    st.button('Next file', on_click=nextFile)
+    
+    st.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
+    st.button('Reset', on_click=resetSeedNumber)
