61de942acf17de16a81fe84678b7d96d52946cbb
frasalvi <salvi.fnc@gmail.com>
2022-05-22 22:21:11 +0200

    remove random seed

--- taxonomy/streamlit_static.py
+++ taxonomy/streamlit_static.py
@@ -14,7 +14,7 @@ def showFile():
     Show the current file, printing the labels and the log.
     '''
 
-    file = st.session_state.files.iloc[st.session_state.fileList[st.session_state.counter]]
+    file = server_state[st.session_state.dataset].iloc[st.session_state.counter]
 
     commons_link = COMMONS_URL + file.url.split('/')[-1]
     image_link = UPLOAD_URL + file.url
@@ -40,7 +40,7 @@ def showFile():
     index_map = {None: 0, 0: 2, 1: 1}
     for label, value in file.labels.items():
         st.radio(label, ('-', 'Correct', 'Wrong'), key=file.title + label, index=index_map[value], 
-                 on_change=evaluate_label, args=(label))
+                 on_change=evaluate_label, args=(file, label))
 
 
     # Extract the current log
@@ -53,33 +53,30 @@ def evaluate_label(file, label):
     index_map = {'Correct': 1, 'Wrong': 0}
     evaluation = index_map[st.session_state[file.title + label]]
     with server_state_lock[st.session_state.dataset]:
-        # server_state[st.session_state.dataset].iloc[st.session_state.fileList[st.session_state.counter]].labels[label] = evaluation
-        file.labels[label]
+        # server_state[st.session_state.dataset].iloc[st.session_state.counter].labels[label]
+        file.labels[label] = evaluation
     showFile()
 
 
-def nextFile():
-    if(st.session_state.counter == len(st.session_state.fileList) - 1):
-        st.session_state.fileList.append(np.random.randint(0, len(st.session_state.files)))
-
-    st.session_state.counter += 1
-    st.session_state.previous_disabled = not bool(st.session_state.counter)
-    showFile()
+# def nextFile():
+#     st.session_state.counter += 1
+#     st.session_state.previous_disabled = not bool(st.session_state.counter)
+#     showFile()
 
 
-def previousFile():
-    st.session_state.counter -= 1
-    if(not st.session_state.counter):
-        st.session_state.previous_disabled = True
-    showFile()
+# def previousFile():
+#     st.session_state.counter -= 1
+#     if(not st.session_state.counter):
+#         st.session_state.previous_disabled = True
+#     showFile()
 
 
-def resetSeedNumber():
-    np.random.seed(st.session_state.seedNumber)
-    st.session_state.fileList = []
-    st.session_state.previous_disabled = True
-    st.session_state.counter = -1
-    nextFile()
+# def resetSeedNumber():
+#     np.random.seed(st.session_state.seedNumber)
+#     st.session_state.fileList = []
+#     st.session_state.previous_disabled = True
+#     st.session_state.counter = -1
+#     nextFile()
     
 
 
@@ -89,41 +86,45 @@ def load_dataset():
             with st.spinner('Loading files...'):
                 server_state[st.session_state.dataset] = pd.read_json(SAMPLE_PATH + st.session_state.dataset)
 
-    st.session_state.files = server_state[st.session_state.dataset]
-    st.session_state.filesize = len(st.session_state.files)
-    resetSeedNumber()
+    st.session_state.filesize = len(server_state[st.session_state.dataset])
+    # resetSeedNumber()
+    showFile()
 
 def save_dataset():
     server_state[st.session_state.dataset].to_json(SAMPLE_PATH + st.session_state.dataset)
+    showFile()
 
 
+def next_unevaluated():
+    evaluated = server_state[st.session_state.dataset].labels.apply(lambda x: None in x.values())
+    evaluated = np.where(evaluated.values[st.session_state.counter+1:])[0]
+    if(evaluated.size):
+        st.session_state.counter += evaluated[0] + 1
+    else:
+        st.warning('All images in this dataset have already been evaluated!')
+    showFile()
 
-# Initialize session
-if 'seedNumber' not in st.session_state:
-    # st.set_page_config(layout="wide")
-    st.session_state.seedNumber = 0
-    st.session_state.dataset = 'files_0.10.json.bz2'
-    # st.text('initializing..')
-    load_dataset()
+
+def main():
+    # Initialize session
+    if 'counter' not in st.session_state:
+        # st.set_page_config(layout="wide")
+        st.session_state.counter = 0
+        st.session_state.previous_disabled = True
+        st.session_state.dataset = 'files_0.10.json.bz2'
+        load_dataset()
 
 
-# col1, col2, col3, col4 = st.columns([1, 1, 1.8, 1.8])
-# col1.text(""); col1.text("");
-# col1.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
-# col2.text(""); col2.text("");
-# col2.button('Next file', on_click=nextFile)
-# col3.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
-# col4.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
-# col4.button('Reset', on_click=resetSeedNumber)
+    with st.sidebar:
+        st.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
+        
+        st.number_input('File counter', min_value=0, max_value=st.session_state.filesize-1, key='counter', on_change=showFile)
+        st.button('Go to next unevaluated image', on_click=next_unevaluated, disabled=(st.session_state.counter==st.session_state.filesize-1))
+        # Blank vertical space
+        for _ in range(10):
+            st.text('')
+        st.button('Save', on_click=save_dataset)
 
-with st.sidebar:
-    st.selectbox('Dataset', options=os.listdir(SAMPLE_PATH) , key='dataset', on_change=load_dataset)
-    st.button('Previous file', key='previousb', on_click=previousFile, disabled=st.session_state.previous_disabled)
-    st.button('Next file', on_click=nextFile)
-    
-    st.number_input('Random seed', min_value=0, key='seedNumber', on_change=showFile)
-    st.button('Go', on_click=resetSeedNumber)
-    # Blank vertical space
-    for _ in range(10):
-        st.text('')
-    st.button('Save', on_click=save_dataset)
+
+if __name__ == "__main__":
+    main()
