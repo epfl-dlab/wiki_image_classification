4836e7d71f0c77f33872ba526922bc45eacf68ba
Matheus Bernat <matheus.bernat@gmail.com>
2022-05-27 11:02:44 +0200

    Model predicting something for all classes

--- classification/test.py
+++
@@ -1,4 +0,0 @@
-﻿filename='/scratch/WIT_Dataset/images/7/7c/Saint-Michel-des-Andaines_-_Prieuré_Saint-Ortaire_-_Chapelle_03.jpg'
-another_file = "/scratch/WIT_Dataset/images/7/7c/--GFDL--Voveraite.JPG"
-with open(filename, "rb") as f:
-    print("hgvhjgvhgvhg")
