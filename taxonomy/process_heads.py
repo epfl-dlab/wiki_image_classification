c63d10484f2b5c432faa9fc8684df9e224baf54b
frasalvi <salvi.fnc@gmail.com>
2022-05-31 20:54:57 +0200

    match sentences instead of heads

--- taxonomy/process_heads.py
+++ taxonomy/process_heads.py
@@ -17,26 +17,6 @@ HEADS_PATH = '/scratch/WikipediaImagesTaxonomy/heads/'
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
-def align_head(categories, heads):
-    '''
-    Align category to its head.
-    Since stanza 1.4. does not support batch processing, the official advice is to concatenate documents
-    with "\n\n". However, this does not respect the boundaries of the original documents, producing potentially more sentences.
-    The function matches categories with their heads, by picking the first head contained in the category.
-    '''
-    category_to_head = {}
-    i_head = 0
-    for i in range(len(categories)):
-        category_to_head[categories[i]] = heads[i_head]
-        i_head += 1
-
-        if(i_head == len(heads)):
-            break
-
-        while(heads[i_head].lower() in categories[i].lower() and heads[i_head].lower() not in categories[i+1].lower()):
-            i_head += 1
-    return category_to_head
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -62,9 +42,8 @@ if __name__ == "__main__":
         categories_batched = np.array_split(categories_chunked[chunk], len(categories_chunked[chunk])//batch_size + 1)
         heads = pd.Series(categories_batched).progress_apply(lambda cs: find_head(cs)).explode().values
         
-        printt('Matching head to category...')
-        heads_dict = align_head(categories_chunked[chunk], heads)
         printt('Saving file...')
+        heads_dict = dict(zip(categories, heads))
 
         with open(HEADS_PATH + f'chunk{chunk}.pkl', 'wb') as fp:
             pickle.dump(heads_dict, fp)
